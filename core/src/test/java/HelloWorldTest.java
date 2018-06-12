30de57a8b26e7f72990598fa81dc9a962199bbcf
lanboys1225 <lanboys1225@gmail.com>
2018-06-12 19:05:33 +0800

    添加core依赖

---
+++ core/src/test/java/HelloWorldTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created by 蓝兵 on 2018/6/12.
+ */
+
+public class HelloWorldTest {
+
+    @Test
+    public void hello() {
+        HelloWorld.main(null);
+    }
+}
