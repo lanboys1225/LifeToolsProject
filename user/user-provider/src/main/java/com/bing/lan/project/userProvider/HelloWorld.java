d2f6c4a7990586f64402af6623d2456db087f0b9
lanboys <lan_bing2013@163.com>
2018-06-12 22:01:55 +0800

    添加user(api/provider)模块

---
+++ user/user-provider/src/main/java/com/bing/lan/project/userProvider/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.bing.lan.project.userProvider;
+
+/**
+ * Created by 蓝兵 on 2018/6/12.
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("main(): Hello javaEE user-provider ! ");
+    }
+}
---
+++ user/user-provider/src/test/java/com/bing/lan/project/userProvider/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package com.bing.lan.project.userProvider;
+
+import org.junit.Test;
+
+/**
+ * Created by 蓝兵 on 2018/6/12.
+ */
+
+public class HelloWorldTest {
+
+    @Test
+    public void hello() {
+        HelloWorld.main(null);
+    }
+}
